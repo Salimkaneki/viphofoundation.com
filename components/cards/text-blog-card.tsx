77a097d823637abddc9c6dbb062a52a71cc3a6da
Salimkaneki <salimpereira01@gmail.com>
2025-12-26 09:03:49 +0000

    the width of text-blog-card

--- components/cards/text-blog-card.tsx
+++ components/cards/text-blog-card.tsx
@@ -7,7 +7,7 @@ interface TextBlogCardProps {
 
 export default function TextBlogCard({date, readTime, title}: TextBlogCardProps) {
     return (
-        <div className="w-[853px] py-10 border-b border-gray-700 text-gray-700 gap-4 flex flex-col cursor-pointer">
+        <div className="w-213.25 py-10 border-b border-gray-700 text-gray-700 gap-4 flex flex-col cursor-pointer">
             <p className="">{date} • {readTime} min de lecture</p>
             <a href="" className="text-gray-700 text-2xl underline  hover:text-primary-400 hover:no-underline">{title}   </a>
         </div>
